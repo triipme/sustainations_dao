252211b37dac160b6e423081afdd9ee678d460e6
kioenguyen0207 <nguyendinhquy2000@gmail.com>
2022-07-18 15:44:20 +0700

    update new sound assets, use fade out for some scene to match game's logic

--- src/sustainations_dao_assets/src/app/main/metaverse/scenes/menuScene.js
+++ src/sustainations_dao_assets/src/app/main/metaverse/scenes/menuScene.js
@@ -15,6 +15,12 @@ const clickSound = "metaverse/audio/Click_sound.mp3";
 const ambientSound = "metaverse/audio/Ambient_sound.mp3";
 const ingameSound = 'metaverse/audio/In_game.mp3';
 const pregameSound = 'metaverse/audio/Pre_game.mp3';
+const sfx_big_waterfall = 'metaverse/audio/SFX_bg_big_waterfall.mp3';
+const sfx_monkey = 'metaverse/audio/SFX_bg_monkey.mp3';
+const sfx_night = 'metaverse/audio/SFX_bg_night.mp3';
+const sfx_small_waterfall = 'metaverse/audio/SFX_bg_small_waterfall.mp3';
+const sfx_char_footstep = 'metaverse/audio/SFX_char_footstep.mp3';
+const sfx_obstacle_remove = 'metaverse/audio/SFX_obstacle_remove.mp3';
 
 class menuScene extends Phaser.Scene {
   constructor() {
@@ -42,6 +48,12 @@ class menuScene extends Phaser.Scene {
     this.load.audio('ambientSound', ambientSound);
     this.load.audio('ingameSound', ingameSound);
     this.load.audio('pregameSound', pregameSound);
+    this.load.audio('sfx_big_waterfall', sfx_big_waterfall);
+    this.load.audio('sfx_monkey', sfx_monkey);
+    this.load.audio('sfx_night', sfx_night);
+    this.load.audio('sfx_small_waterfall', sfx_small_waterfall);
+    this.load.audio('sfx_char_footstep', sfx_char_footstep);
+    this.load.audio('sfx_obstacle_remove', sfx_obstacle_remove);
 
     // preload
     this.load.image("bg", bg);
--- src/sustainations_dao_assets/src/app/main/metaverse/scenes/scene5.js
+++ src/sustainations_dao_assets/src/app/main/metaverse/scenes/scene5.js
@@ -84,6 +84,8 @@ export default class Scene5 extends Phaser.Scene {
     this.ingameSound.isRunning = false;
     this.ambientSound = this.sound.add('ambientSound', {loop: true});
     this.ambientSound.play();
+    this.sfx_char_footstep = this.sound.add('sfx_char_footstep', {loop: true, volume: 0.2});
+    this.sfx_char_footstep.play();
     //background
     this.bg_1 = this.add.tileSprite(0, 0, gameConfig.scale.width, gameConfig.scale.height, "background1");
     this.bg_1.setOrigin(0, 0);
@@ -130,11 +132,11 @@ export default class Scene5 extends Phaser.Scene {
     this.bg_3.setScrollFactor(0);
 
     //UI
-    this.add.image(20, 40, "UI_NameCard").setOrigin(0).setScrollFactor(0).setScale(0.95);
-    this.add.image(370, 40, "UI_HP").setOrigin(0).setScrollFactor(0).setScale(0.95);
-    this.add.image(720, 40, "UI_Mana").setOrigin(0).setScrollFactor(0).setScale(0.95);
-    this.add.image(1070, 40, "UI_Stamina").setOrigin(0).setScrollFactor(0).setScale(0.95);
-    this.add.image(1420, 40, "UI_Morale").setOrigin(0).setScrollFactor(0).setScale(0.95);
+    this.add.image(20, 40, "UI_NameCard").setOrigin(0).setScrollFactor(0);
+    this.add.image(370, 40, "UI_HP").setOrigin(0).setScrollFactor(0);
+    this.add.image(720, 40, "UI_Mana").setOrigin(0).setScrollFactor(0);
+    this.add.image(1070, 40, "UI_Stamina").setOrigin(0).setScrollFactor(0);
+    this.add.image(1420, 40, "UI_Morale").setOrigin(0).setScrollFactor(0);
     this.add.image(80, 830, "UI_Utility").setOrigin(0).setScrollFactor(0);
     this.add.image(1780, 74, "BtnExit").setOrigin(0).setScrollFactor(0).setScale(0.7)
       .setInteractive()
@@ -143,7 +145,7 @@ export default class Scene5 extends Phaser.Scene {
         this.scene.start('menuScene');
         this.ambientSound.stop();
         this.ingameSound.stop();
-
+        this.sfx_char_footstep.stop();
       });
 
     //mycam
@@ -179,6 +181,7 @@ export default class Scene5 extends Phaser.Scene {
       });
       this.options[idx].on('pointerdown', () => {
         this.triggerContinue();
+        this.sfx_char_footstep.play();
         this.clickSound.play();
       });
     }
@@ -192,12 +195,14 @@ export default class Scene5 extends Phaser.Scene {
 
     if (this.player.x > 1920*4) {
       this.ingameSound.stop();
+      this.sfx_char_footstep.stop();
       this.scene.start("Scene6");
     }
 
     if (this.player.x > 1920*3 - 300 && this.isInteracted == false) {
       this.triggerPause();
       this.ambientSound.stop();
+      this.sfx_char_footstep.stop();
       if (this.ingameSound.isRunning == false) {
         this.ingameSound.play();
         this.ingameSound.isRunning = true;
