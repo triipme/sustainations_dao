e4f0b5caab467cfcb028f6f1ecd5927445e7f416
Mavis2103 <taquanvku@gmail.com>
2022-08-30 17:43:55 +0700

    seperate 'menu' scene from phaser to react

--- src/sustainations_dao_assets/src/app/main/metaverse/Metaverse.js
+++ src/sustainations_dao_assets/src/app/main/metaverse/Metaverse.js
@@ -1,22 +1,90 @@
-import gameConfig from "./GameConfig";
-import GetData from "./GetData";
-class Metaverse extends React.Component {
-  componentDidMount() {
-    this.slug = new Phaser.Game(gameConfig);
-    document.body.classList.add("black-bg");
-  }
+import { styled } from "@mui/material/styles";
+import history from "@history";
+import { Box, Stack } from "@mui/material";
+import { useEffect } from "react";
 
-  shouldComponentUpdate() {
-    return false;
+const btnWidth = 693;
+const btnHeight = 163;
+const xs = 0.17;
+const sm = 0.3;
+const lg = 0.5;
+const xl = 0.6;
+const Metaverse = () => {
+  function handleClick(e) {
+    history.replace("/metaverse");
   }
-
-  componentWillUnmount() {
-    document.body.classList.remove("black-bg");
-  }
-
-  render() {
-    return <div id="sustainations-slug"></div>;
-  }
-}
-
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "grid",
+        placeItems: "center",
+        backgroundColor: "#000"
+      }}>
+      <Box
+        // component="img"
+        // src="metaverse/UI_finish.png"
+        sx={{
+          // objectFit: "contain",
+          // m: "auto",
+          // height: { xs: "auto", md: "100vh" },
+          width: "100%",
+          height: "100vh",
+          backgroundImage: "url(metaverse/menu/Background.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "contain",
+          display: "grid",
+          placeItems: "center"
+        }}>
+        <Stack justifyContent="center" alignItems="center">
+          <Box
+            component="img"
+            src="metaverse/menu/welcome.png"
+            sx={{ objectFit: "cover", width: { xs: "50%", md: "80%" } }}
+          />
+          <Item
+            src="metaverse/menu/introduction.png"
+            onClick={() => window.open("https://www.youtube.com/watch?v=ZgwDobu5OcY", "_blank")}
+          />
+          <Item src="metaverse/menu/quest.png" onClick={() => history.push("/metaverse/quests")} />
+          <Item
+            src="metaverse/menu/bootcamp.png"
+            onClick={() => history.push("/metaverse/bootcamp")}
+          />
+          <Item src="metaverse/menu/land.png" onClick={() => history.push("/metaverse/land")} />
+          <Item src="metaverse/menu/departure.png" onClick={() => history.push("/")} />
+        </Stack>
+      </Box>
+    </Box>
+  );
+};
 export default Metaverse;
+const Item = props => {
+  return (
+    <Box
+      component="img"
+      {...props}
+      sx={{
+        objectFit: "cover",
+        height: {
+          xs: btnHeight * xs,
+          sm: btnHeight * sm,
+          lg: btnHeight * lg,
+          xl: btnHeight * xl
+        },
+        width: {
+          xs: btnWidth * xs,
+          sm: btnWidth * sm,
+          lg: btnWidth * lg,
+          xl: btnWidth * xl
+        },
+        objectPosition: "0% 0%",
+        "&:hover": {
+          objectPosition: "100% 0%"
+        }
+      }}
+    />
+  );
+};
