5cb2b085675bb03b679de1ad478a8e0637670929
hoangkhacs <khavh.cs@gmail.com>
2022-10-27 19:08:12 +0700

    add database example

--- scripts/data_engine.sh
+++
@@ -1,3 +0,0 @@
-dfx canister call sustainations_dao createQuestEngine '(record{id="q1";name="Quest Engine";description="Test Description";image="Lake.png"})'
-
-dfx canister call sustainations_dao createQuestEngine '(record{})'
