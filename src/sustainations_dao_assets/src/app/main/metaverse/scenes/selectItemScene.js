bdd5ba55ad03ab57ae9e846b46e6c2154f62a238
thanhduy5112020 <thanhduy5112020@gmail.com>
2022-10-27 10:03:53 +0700

    engine bug

--- src/sustainations_dao_assets/src/app/main/metaverse/scenes/selectItemScene.js
+++ src/sustainations_dao_assets/src/app/main/metaverse/scenes/selectItemScene.js
@@ -29,8 +29,6 @@ const UI_NameCard = 'metaverse/selectItems/UI_id_name.png';
 const player = 'metaverse/selectItems/UI_player.png';
 const pickItemText = 'metaverse/selectItems/UI_pick_item.png';
 
-const sum = 1;
-
 class selectItemScene extends BaseScene {
   constructor() {
     super('selectItemScene');
@@ -208,6 +206,9 @@ class selectItemScene extends BaseScene {
         case 'lava':
           this.scene.start('lava_scene1');
           break;
+        // case 'lake':
+        //   this.scene.start('lake_scene1');
+        //   break;
         case 'lake':
           this.scene.start('BaseEngine', { listScene: await listSceneQuests("lake") });
           break;
