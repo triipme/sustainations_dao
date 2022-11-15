d05db86f1ec6fcaa562f078cb091086b86bdd6c3
Puteors <nguyentriphuc30112001@gmail.com>
2022-11-15 20:47:04 +0700

    exit button farm

--- src/sustainations_dao_assets/src/app/main/metaverse/land/FarmUI.js
+++ src/sustainations_dao_assets/src/app/main/metaverse/land/FarmUI.js
@@ -3,7 +3,7 @@ import "./styles.css";
 const UIFarm = () => {
   return (
     <>
-      <ul style={{ zIndex: 10000, position: "fixed", width: "100vw", margin: "2vh", top: "20px" }}>
+      <ul style={{ zIndex: 10000, position: "fixed", width: "80vw", margin: "2vh", top: -20 }}>
         <li>
           <div>
           <img src="metaverse/farm/Sustaination_farm/decor-object/PNG/Sustaination__farm-object-27.png"></img>
@@ -18,9 +18,9 @@ const UIFarm = () => {
         <li>
           <img src="metaverse/farm/Sustaination_farm/decor-object/PNG/Sustaination__farm-object-30.png"></img>
         </li>
-        <li>
+        {/* <li>
           <img src="metaverse/farm/Sustaination_farm/decor-object/PNG/Sustaination__farm-object-31.png"></img>
-        </li>
+        </li> */}
       </ul>
       <div className="navBar">
         <img src="metaverse/farm/Sustaination_farm/decor-object/PNG/Sustaination__farm-object-32.png"></img>
